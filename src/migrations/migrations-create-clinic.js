cacf681aa2ab34b84495317d5b2a8e6a1f2ba83c
Nam Nguyen <son2life@gmail.com>
2023-09-17 14:57:28 +0700

    update 4 truong

--- src/migrations/migrations-create-clinic.js
+++ src/migrations/migrations-create-clinic.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     // statusId: DataTypes.STRING,
@@ -6,33 +6,36 @@ module.exports = {
     // patientId: DataTypes.INTEGER,
     // date: DataTypes.DATE,
     // timeType: DataTypes.STRING
-    await queryInterface.createTable('clinics', {
+    await queryInterface.createTable("clinics", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
-      address:  {
-        type: Sequelize.STRING
+      name: {
+        type: Sequelize.STRING,
+      },
+      address: {
+        type: Sequelize.STRING,
       },
       description: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
       },
       image: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('clinics');
-  }
+    await queryInterface.dropTable("clinics");
+  },
 };
