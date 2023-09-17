00a2630d1dacae7dc0ee2abc0cc34519ac564d58
Nam Nguyen <son2life@gmail.com>
2023-09-17 14:49:18 +0700

    17/09

---
+++ src/migrations/migrations-create-specialty.js
@@ -0,0 +1,35 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    // statusId: DataTypes.STRING,
+    // doctorId: DataTypes.INTEGER,
+    // patientId: DataTypes.INTEGER,
+    // date: DataTypes.DATE,
+    // timeType: DataTypes.STRING
+    await queryInterface.createTable("specialties", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      description: {
+        type: Sequelize.TEXT,
+      },
+      image: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("specialties");
+  },
+};
---
+++ src/seeders/seeder-user.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert("Users", [
+      {
+        email: "admin1@example.com",
+        password: "123456",
+        firstName: "John",
+        lastName: "Doe",
+        address: "184 Hoang Van Thu",
+        gender: 1,
+        typeRole: "ROLE",
+        keyRole: "R1",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
