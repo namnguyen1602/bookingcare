0c39d402d3bf8d20a69a1941f1ff9855a20c62ef
Nam Nguyen <son2life@gmail.com>
2023-09-18 19:51:59 +0700

    18/09

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,25 +1,52 @@
-import db from '../models/index';
+import db from "../models/index";
+import CRUDservice from "../services/CRUDservice";
 let getHomePage = async (req, res) => {
-    try{
-        let data = await db.User.findAll();
-        console.log(data)
-        return res.render('homepage.ejs' ,{
-            data: JSON.stringify(data)
-        });
-    }catch(e){
-        console.log(e)
-    }
-}
+	try {
+		let data = await db.User.findAll();
+		console.log(data);
+		return res.render("homepage.ejs", {
+			data: JSON.stringify(data),
+		});
+	} catch (e) {
+		console.log(e);
+	}
+};
 
 let getAboutPage = (req, res) => {
-    return res.render('test/about.ejs');
-}
+	return res.render("test/about.ejs");
+};
+let getCRUD = (req, res) => {
+	return res.render("crud.ejs");
+};
+let postCRUD = async (req, res) => {
+	let message = await CRUDservice.createNewUser(req.body);
+	console.log(message);
+	return res.send("post crud from server");
+};
 
-// object:{
-//     key:'',
-//     value: ''
-// }
+let displayGetCRUD = async (req, res) => {
+	let data = await CRUDservice.getAllUser();
+	console.log(data);
+	return res.render("displayCRUD.ejs", {
+		dataTable: data,
+	});
+};
+
+let getEditCRUD = async (req, res) => {
+	let userId = req.query.id;
+	if (userId) {
+		let userData = CRUDservice.getUserInforById(userId);
+		return res.send("Found a user ");
+	} else {
+		return res.send("User not found");
+	}
+};
+
 module.exports = {
-    getHomePage: getHomePage,
-    getAboutPage: getAboutPage
-}
+	getHomePage: getHomePage,
+	getAboutPage: getAboutPage,
+	getCRUD: getCRUD,
+	postCRUD: postCRUD,
+	displayGetCRUD: displayGetCRUD,
+	getEditCRUD: getEditCRUD,
+};
--- src/route/web.js
+++ src/route/web.js
@@ -3,12 +3,15 @@ import homeController from "../controller/homeController";
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get('/', homeController.getHomePage );
+	router.get("/", homeController.getHomePage);
 
-    router.get('/about', homeController.getAboutPage);
+	router.get("/about", homeController.getAboutPage);
+	router.get("/crud", homeController.getCRUD);
 
-
-    return app.use("/", router);
-}
+	router.post("/post-crud", homeController.postCRUD);
+	router.get("/get-crud", homeController.displayGetCRUD);
+	router.get("/edit-crud", homeController.getEditCRUD);
+	return app.use("/", router);
+};
 
 module.exports = initWebRoutes;
